380aabc3a88c7a4f7b80f68c6947c2fc89d1d46b
suvankars <suvankar.17@gmail.com>
2016-09-05 12:46:18 +0530

    Home page configured

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,7 +62,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    registrations_instruction_path
+    #registrations_instruction_path
     #super(resource)
   end
 
